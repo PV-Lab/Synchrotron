2dcbfb283b35f0165141c914a308ef7d02a6347d
maljensen <jensenma@mit.edu>
2017-01-10 11:49:01 -0500

    Adding thresholding of XRF images

--- plots_for_publishing.m
+++ plots_for_publishing.m
@@ -25,10 +25,12 @@ SOFTWARE.
 clear all; close all; clc;
 
 %Read in x,y particle data for each sigma 
-filename = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\particle_details.xlsx';
+filename = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\particle_details.xlsx';
 
-data = xlsread(filename); 
-codes = {'187 AG' '188 AG' '187 G1' '187 G2' '187 G3' '188 G1' '188 G2'};
+data_Cu = xlsread(filename,'Cu'); 
+data_Ni = xlsread(filename,'Ni'); 
+%scan numbers in order of our analysis
+scans = [189,190,204,205,206,213,102];
 
 %Make plots for publication. 
 radius = 0.00075; 
@@ -41,30 +43,35 @@ cutoff_min = 88;
 cutoff_max = 95;
 
 %SAL-1 (scans 189, 190)
-Cu_SAL1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0189.h5.txt'; 
-Cu_SAL1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0190.h5.txt'; 
-Ni_SAL1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0189.h5.txt'; 
-Ni_SAL1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0190.h5.txt'; 
-elastic_SAL1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0189.h5.txt'; 
-elastic_SAL1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0190.h5.txt'; 
+Cu_SAL1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0189.h5.txt'; 
+Cu_SAL1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0190.h5.txt'; 
+Ni_SAL1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0189.h5.txt'; 
+Ni_SAL1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0190.h5.txt'; 
+elastic_SAL1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0189.h5.txt'; 
+elastic_SAL1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0190.h5.txt'; 
 
 %SAH-1 (scans 204, 205)
-Cu_SAH1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0204.h5.txt'; 
-Cu_SAH1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0205.h5.txt'; 
-Ni_SAH1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0204.h5.txt'; 
-Ni_SAH1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0205.h5.txt'; 
-elastic_SAH1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0204.h5.txt'; 
-elastic_SAH1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0205.h5.txt'; 
+Cu_SAH1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0204.h5.txt'; 
+Cu_SAH1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Cu_2idd_0205.h5.txt'; 
+Ni_SAH1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0204.h5.txt'; 
+Ni_SAH1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_Ni_2idd_0205.h5.txt'; 
+elastic_SAH1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0204.h5.txt'; 
+elastic_SAH1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\with Filt 1\output\ASCII_s_e_2idd_0205.h5.txt'; 
 
 %S-1 as-grown (2016c2 scans 206 for Cu/Ni, scan 213 for Ti)
-Cu_S1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Cu_2idd_0206.h5.txt'; 
-Cu_S1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Cu_2idd_0213.h5.txt'; 
-Ni_S1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ni_2idd_0206.h5.txt'; 
-Ni_S1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ni_2idd_0213.h5.txt'; 
-elastic_S1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_s_e_2idd_0206.h5.txt'; 
-elastic_S1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_s_e_2idd_0213.h5.txt'; 
-Ti_S1_1 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ti_2idd_0206.h5.txt'; 
-Ti_S1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ti_2idd_0213.h5.txt'; 
+Cu_S1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Cu_2idd_0206.h5.txt'; 
+Cu_S1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Cu_2idd_0213.h5.txt'; 
+Ni_S1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ni_2idd_0206.h5.txt'; 
+Ni_S1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ni_2idd_0213.h5.txt'; 
+elastic_S1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_s_e_2idd_0206.h5.txt'; 
+elastic_S1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_s_e_2idd_0213.h5.txt'; 
+Ti_S1_1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ti_2idd_0206.h5.txt'; 
+Ti_S1_2 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\output\ASCII_Ti_2idd_0213.h5.txt'; 
+
+%Phosphorous diffusion PS1 (2016c3 scan 102)
+Cu_PS1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\no filter\output\ASCII_Cu_2idd_0102.h5.txt'; 
+Ni_PS1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\no filter\output\ASCII_Ni_2idd_0102.h5.txt'; 
+elastic_PS1 = 'C:\Users\Mariela\Documents\LeTID\XRF\2-ID-D 2016c3\fitted\no filter\output\ASCII_s_e_2idd_0102.h5.txt'; 
 
 %Get all of the maps for SAL-1
 [mapSAL1_1_Cu] = processAsciiFile(Cu_SAL1_1,cutoff_flag,cutoff_min,cutoff_max);
@@ -91,6 +98,11 @@ Ti_S1_2 = 'C:\Users\Malloryj\Documents\LeTID\XRF\2-ID-D 2016c2\MIT.16c2.img.dat\
 [mapS1_2_elastic] = processAsciiFile(elastic_S1_2,cutoff_flag,cutoff_min,cutoff_max);
 [mapS1_1_Ti] = processAsciiFile(Ti_S1_1,cutoff_flag,cutoff_min,cutoff_max);
 [mapS1_2_Ti] = processAsciiFile(Ti_S1_2,cutoff_flag,cutoff_min,cutoff_max);
+
+%Get all of the maps for PS1
+[mapPS1_Cu] = processAsciiFile(Cu_PS1,cutoff_flag,cutoff_min,cutoff_max);
+[mapPS1_Ni] = processAsciiFile(Ni_PS1,cutoff_flag,cutoff_min,cutoff_max);
+[mapPS1_elastic] = processAsciiFile(elastic_PS1,cutoff_flag,cutoff_min,cutoff_max);
 
 %% SAL-1
 
@@ -143,6 +155,47 @@ set(gca, 'XTick', []);
 set(gca, 'YTick', []);
 axis off;
 
+%Get the particle information
+index_Cu = find(data_Cu(:,1)==scans(1));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+SAL1_1_Cu = imbinarize(mapSAL1_1_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(1));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+SAL1_1_Ni = imbinarize(mapSAL1_1_Ni.counts,threshold_Ni(1)); 
+Cu_thresh = figure; 
+image(mapSAL1_1_Cu.xValue,mapSAL1_1_Cu.yValue,SAL1_1_Cu,'CDataMapping','scaled');
+Ni_thresh = figure; 
+image(mapSAL1_1_Ni.xValue,mapSAL1_1_Ni.yValue,SAL1_1_Ni,'CDataMapping','scaled');
+
+%Repeat the above for the second scan on this sample
+index_Cu = find(data_Cu(:,1)==scans(2));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+SAL1_2_Cu = imbinarize(mapSAL1_2_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(2));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+SAL1_2_Ni = imbinarize(mapSAL1_2_Ni.counts,threshold_Ni(1)); 
+figure(Cu_thresh)
+hold all; 
+image(mapSAL1_2_Cu.xValue,mapSAL1_2_Cu.yValue,SAL1_2_Cu,'CDataMapping','scaled');
+axis('image'); 
+figure(Ni_thresh);
+hold all; 
+image(mapSAL1_2_Ni.xValue,mapSAL1_2_Ni.yValue,SAL1_2_Ni,'CDataMapping','scaled');
+axis('image'); 
 %% SAH-1
 
 %Cu first
@@ -196,6 +249,47 @@ set(gca, 'XTick', []);
 set(gca, 'YTick', []);
 axis off;
 
+%Get the particle information
+index_Cu = find(data_Cu(:,1)==scans(3));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+SAH1_1_Cu = imbinarize(mapSAH1_1_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(3));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+SAH1_1_Ni = imbinarize(mapSAH1_1_Ni.counts,threshold_Ni(1)); 
+Cu_thresh = figure; 
+image(mapSAH1_1_Cu.xValue,mapSAH1_1_Cu.yValue,SAH1_1_Cu,'CDataMapping','scaled');
+Ni_thresh = figure; 
+image(mapSAH1_1_Ni.xValue,mapSAH1_1_Ni.yValue,SAH1_1_Ni,'CDataMapping','scaled');
+
+%Repeat the above for the second scan on this sample
+index_Cu = find(data_Cu(:,1)==scans(4));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+SAH1_2_Cu = imbinarize(mapSAH1_2_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(4));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+SAH1_2_Ni = imbinarize(mapSAH1_2_Ni.counts,threshold_Ni(1)); 
+figure(Cu_thresh)
+hold all; 
+image(mapSAH1_2_Cu.xValue,mapSAH1_2_Cu.yValue,SAH1_2_Cu,'CDataMapping','scaled');
+axis('image'); 
+figure(Ni_thresh);
+hold all; 
+image(mapSAH1_2_Ni.xValue,mapSAH1_2_Ni.yValue,SAH1_2_Ni,'CDataMapping','scaled');
+axis('image'); 
 %% S1 2016c2 - these we plot separately because they are in different locations
 
 %Cu first
@@ -298,6 +392,89 @@ set(gca, 'XTick', []);
 set(gca, 'YTick', []);
 axis off;
 
+%Get the particle information
+index_Cu = find(data_Cu(:,1)==scans(5));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+S1_1_Cu = imbinarize(mapS1_1_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(5));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+S1_1_Ni = imbinarize(mapS1_1_Ni.counts,threshold_Ni(1)); 
+Cu_thresh = figure; 
+image(mapS1_1_Cu.xValue,mapS1_1_Cu.yValue,S1_1_Cu,'CDataMapping','scaled');
+axis('image'); 
+Ni_thresh = figure; 
+image(mapS1_1_Ni.xValue,mapS1_1_Ni.yValue,S1_1_Ni,'CDataMapping','scaled');
+axis('image'); 
+
+%% PS1
+
+%Cu first
+% logged = log(mapPS1_Cu.counts);
+hFig=figure;
+% image(mapPS1_Cu.xValue,mapPS1_Cu.yValue,logged,'CDataMapping','scaled');
+image(mapPS1_Cu.xValue,mapPS1_Cu.yValue,mapPS1_Cu.counts,'CDataMapping','scaled');
+% colormap(flipud(gray));
+colormap(parula);
+axis image; 
+caxis([0 0.01]);
+set(gca, 'XTick', []);
+set(gca, 'YTick', []);
+axis off;
+
+%Ni next
+% logged = log(mapPS1_Ni.counts);
+hFig=figure;
+% image(mapPS1_Ni.xValue,mapPS1_Ni.yValue,logged,'CDataMapping','scaled');
+image(mapPS1_Ni.xValue,mapPS1_Ni.yValue,mapPS1_Ni.counts,'CDataMapping','scaled');
+% colormap(flipud(gray));
+colormap(parula);
+axis image; 
+caxis([0 0.008]);
+set(gca, 'XTick', []);
+set(gca, 'YTick', []);
+axis off;
+
+%elastic next
+% logged = log(mapPS1_elastic.counts);
+hFig=figure;
+% image(mapPS1_elastic.xValue,mapPS1_elastic.yValue,logged,'CDataMapping','scaled');
+image(mapPS1_elastic.xValue,mapPS1_elastic.yValue,mapPS1_elastic.counts,'CDataMapping','scaled');
+% colormap(flipud(gray));
+colormap(parula);
+axis image; 
+caxis([821 1479]);
+set(gca, 'XTick', []);
+set(gca, 'YTick', []);
+axis off;
+
+%Get the particle information
+index_Cu = find(data_Cu(:,1)==scans(7));
+x_Cu = data_Cu(index_Cu,2);
+y_Cu = data_Cu(index_Cu,3);
+threshold_Cu = data_Cu(index_Cu,8); %Choose the noise limit
+%Make the new thresholded image
+PS1_Cu = imbinarize(mapPS1_Cu.counts,threshold_Cu(1)); 
+%Do the same for Ni
+index_Ni = find(data_Ni(:,1)==scans(7));
+x_Ni = data_Ni(index_Ni,2);
+y_Ni = data_Ni(index_Ni,3);
+threshold_Ni = data_Ni(index_Ni,8); %Choose the noise limit
+%Make the new thresholded image
+PS1_Ni = imbinarize(mapPS1_Ni.counts,threshold_Ni(1)); 
+Cu_thresh = figure; 
+image(mapPS1_Cu.xValue,mapPS1_Cu.yValue,PS1_Cu,'CDataMapping','scaled');
+axis('image'); 
+Ni_thresh = figure; 
+image(mapPS1_Ni.xValue,mapPS1_Ni.yValue,PS1_Ni,'CDataMapping','scaled');
+axis('image'); 
+
 %% 187 As-grown
 
 %Pick out the relevant x,y data for this scan
@@ -345,34 +522,5 @@ set(hFig,'Position',[0 0 n*macroscale m*macroscale]);
 box on;
 print('-dpng','-r0','187AsGrown_sig4_pub');
 
-%% Use the elastic maps to estimate the grain boundary length (will be used to estimate precipitate density)
-
-figure(EAG187);
-disp('Select the y-location of the left edge of the grain boundary and then the right edge of the grain boundary');
-[x,y] = ginput(2); 
-dx = abs(x(1)-x(2));
-dy = abs(y(1)-y(2));
-EAG187_length = sqrt((dx^2)+(dy^2))
-
-figure(EAG188);
-disp('Select the y-location of the left edge of the grain boundary and then the right edge of the grain boundary');
-[x,y] = ginput(2); 
-dx = abs(x(1)-x(2));
-dy = abs(y(1)-y(2));
-EAG188_length = sqrt((dx^2)+(dy^2))
-
-figure(EGett187);
-disp('Select the y-location of the left edge of the grain boundary and then the right edge of the grain boundary');
-[x,y] = ginput(2); 
-dx = abs(x(1)-x(2));
-dy = abs(y(1)-y(2));
-EGett187_length = sqrt((dx^2)+(dy^2))
-
-figure(EGett188);
-disp('Select the y-location of the left edge of the grain boundary and then the right edge of the grain boundary');
-[x,y] = ginput(2); 
-dx = abs(x(1)-x(2));
-dy = abs(y(1)-y(2));
-EGett188_length = sqrt((dx^2)+(dy^2))
 
 
