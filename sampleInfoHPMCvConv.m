49f11397a727eef946f8cafc6bd62b021dac795a
maljensen <jensenma@mit.edu>
2017-10-29 19:51:17 -0400

    specifying new scan parameters

---
+++ sampleInfoHPMCvConv.m
@@ -0,0 +1,37 @@
+%{
+MIT License
+
+Copyright (c) [2016] [Mallory Ann Jensen, jensenma@alum.mit.edu]
+Note: File contents from David P. Fenning. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+%}
+
+%% sampleInfoSERISCuNi.m
+% Modified by Mallory Jensen 2017 Jan 6
+
+%%
+
+Samples = {'C132_RA18_1','C132_RA18_2','R697_RA18_1','R697_RA18_2','C132_sig9_38'}; %Exclude the last 189 scan because no particles were found
+hiResScans = {'0089','0091','0109','0115','0127'}; 
+
+channels = {'Si','Fe','Cu','Ni','s_e'};
+
+
+%% END
