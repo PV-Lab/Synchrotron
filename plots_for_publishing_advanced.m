3f77fdf9caa95e48e74620fff5303d38a4c0786a
maljensen <jensenma@mit.edu>
2017-11-14 11:43:55 -0500

    adding thresholds

--- plots_for_publishing_advanced.m
+++ plots_for_publishing_advanced.m
@@ -51,9 +51,20 @@ samp_2017c1 = {{'0032'},{'0038'},{'0039'}};
 name_2017c1 = {'137 ROI 1','137 ROI 2 - 1','137 ROI 2 - 2'};
 cax_2017c1 = {{[],[],[],[],[]},{[],[],[],[],[]},{[],[],[],[],[]},{[],[],[],[],[]}};
 
+%Threshold?
+threshold = 'Y'; 
+thresholds_2016c3 = {{[],[],[7.553132849],[3.259183339],[4.280697346]},...
+    {[],[],[7.553132849],[3.259183339],[4.280697346]},...
+    {[],[],[9.250660719],[3.99166808],[5.242762121]}};
+thresholds_2017c1 = {{[],[],[8.587926147],[4.348457702],[5.359043335]},...
+    {[],[],[23.51900436],[11.90874187],[14.67634461]},...
+    {[],[],[33.26089495],[16.84150426],[20.75548559]}};
+
+
 
 %How many metals do we have
-metals = {'s_e','Si','Fe','Cu','Ni','Co','Ca'}; 
+% metals = {'s_e','Si','Fe','Cu','Ni','Co','Ca'}; 
+metals = {'s_e','Si','Fe','Cu','Ni'}; 
 %Let's read the particle data'
 % for i = 1:length(metals)
 %     if strcmp(metals{i},'s_e')==0 && strcmp(metals{i},'Si')==0
@@ -75,6 +86,7 @@ cutoff_max = 95;
 all_samp = {samp_2016c3,samp_2017c1}; 
 all_names = {name_2016c3,name_2017c1}; 
 all_cax = {cax_2016c3,cax_2017c1};
+all_thresholds = {thresholds_2016c3,thresholds_2017c1}; 
 
 if strcmp(density_calc,'Y')==1
     %Prep for gathering the line density information
@@ -92,6 +104,7 @@ for i = 1:no_runs
     dir_thisrun = all_dir{i}; 
     samples_thisrun = all_samp{i}; 
     names_thisrun = all_names{i}; 
+    thresholds_now = all_thresholds{i}; 
     no_samp = length(samples_thisrun); 
     density_store_run = cell(no_samp,1); 
     cax_thisrun = all_cax{i}; 
@@ -99,13 +112,14 @@ for i = 1:no_runs
        scans_thisrun = samples_thisrun{j}; 
        no_scans = length(scans_thisrun); 
        cax_thissample = cax_thisrun{j}; 
+       thresholds_thissample = thresholds_now{j}; 
        if strcmp(density_calc,'Y')==1
            %We want to look at the line density
            density = zeros(length(size_bins)-1,length(metals)-2); 
        end
        for k = 1:length(metals)
            %We'll make one figure together for these scans
-           figure; 
+           pubmap = figure; 
            for m = 1:no_scans
                %make the filename
                filename = [dir_thisrun '\ASCII_' metals{k} '_2idd_' ...
@@ -157,7 +171,17 @@ for i = 1:no_runs
                         density(sz,k-2) = length(parts_bin)/GB_length(index_length,2); %ppt/micron
                     end
                end 
+               %Make the threshold images
+               if strcmp(threshold,'Y')==1 && strcmp(metals{k},'s_e')==0 &&...
+                       strcmp(metals{k},'Si')==0
+                    threshold_now = thresholds_thissample{k}; 
+                    %Make the new thresholded image
+                    thresh_map = imbinarize((easy_maps.counts).*1e-6.*1e9,threshold_now); 
+                    thresh_fig = figure; 
+                    image(easy_maps.xValue,easy_maps.yValue,thresh_map,'CDataMapping','scaled');
+               end
            end
+           figure(pubmap)
            %Now we're done plotting all of the scans
            colormap(parula); 
            axis image; 
@@ -173,6 +197,19 @@ for i = 1:no_runs
            set(fig,'PaperPositionMode','auto');
            print(fig,'-dpng','-r0',[dir_thisrun '\' names_thisrun{j} '_' metals{k} '.png']); 
            savefig(fig,[dir_thisrun '\' names_thisrun{j} '_' metals{k} '.fig']);
+           if strcmp(threshold,'Y')==1 && strcmp(metals{k},'s_e')==0 &&...
+                       strcmp(metals{k},'Si')==0
+               %Same for threshold
+               figure(thresh_fig); 
+               colormap(parula); 
+               axis image; 
+               set(gca, 'XTick', []);
+               set(gca, 'YTick', []);
+               axis off;
+               set(thresh_fig,'PaperPositionMode','auto');
+               print(thresh_fig,'-dpng','-r0',[dir_thisrun '\' names_thisrun{j} '_' metals{k} '_threshold.png']); 
+               savefig(thresh_fig,[dir_thisrun '\' names_thisrun{j} '_' metals{k} '_threshold.fig']);
+           end
        end
        if strcmp(density_calc,'Y')==1
            density_storerun{j} = density; 
